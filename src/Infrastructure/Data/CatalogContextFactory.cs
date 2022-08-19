8920fb8ef3c1b945003c47c525994eafc7cd524a
nbiyanka <Biyanka.Perera@outlook.com>
2022-08-20 11:16:08 +1200

    Updated Nuget packages
    SQL server db from InMemory database

---
+++ src/Infrastructure/Data/CatalogContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data;
+public class CatalogContextFactory : IDesignTimeDbContextFactory<CatalogContext>
+{
+    public CatalogContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CatalogContext>();
+        optionsBuilder.UseSqlServer("Server=.\\SQL2019;Integrated Security=true;Initial Catalog=eShopOnWeb");
+
+        return new CatalogContext(optionsBuilder.Options);
+    }
+}
